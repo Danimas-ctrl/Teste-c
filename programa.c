7ef6f84e12be6207c0504f303661b30036eb5fb6
Danimas-ctrl <dani.cella.mascarenhas@gmail.com>
2026-03-19 12:43:01 -0300

    Update programa.c

--- programa.c
+++ programa.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    printf("Olá, Codespaces!\n");
-    return 0;
-}
+int main()
+{
+  int N;
+  scanf("%d", &N);
 
+  while (N > 0) {
+    printf("%d\n", N % 10);
+    N /= 10; /* equivalente a N = N / 10; */
+  }
+
+  return 0;
+}
